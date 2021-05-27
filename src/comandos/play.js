039db45267bbad3a774e2eec887bc81f8ce3d3d8
Leonardo Eugenio Mascarenhas Mantoni <51415831+leonardoEugenio@users.noreply.github.com>
2021-05-26 22:28:53 -0300

    pegado informações de membros de um voice channel

--- src/comandos/play.js
+++ src/comandos/play.js
@@ -1,8 +1,12 @@
+
 async function play(msg, bot) {
     const msgAutor = msg.member
     if (!msgAutor.voice.channel)
         return msg.reply("Entre em um canal de voz, em que você deseja jogar")
 
     msg.reply("Calma ae to arrumando umas coizinhas aqui;\n O jogo ja vai começar :D");
+
+    let channel = msg.guild.channels.cache.get(msgAutor.voice.channel.id);
+    console.log(channel.members);
 }
 module.exports = play;
