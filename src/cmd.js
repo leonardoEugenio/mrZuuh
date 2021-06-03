8345265eb9e97457bbadbe2f6ab6cf9ec0fdffce
Leonardo Eugenio Mascarenhas Mantoni <51415831+leonardoEugenio@users.noreply.github.com>
2021-06-03 01:10:23 -0300

    possibilita passar parametros a os comandos

--- src/cmd.js
+++ src/cmd.js
@@ -2,8 +2,9 @@ const ConfigDs = require('./configDS.json');
 const commands = {
     "ajuda"    : require('./comandos/ajuda'),
     "limpar"   : require('./comandos/limpar'),
-    "ng"  : require('./comandos/newGame'),
-    "join"     : require('./comandos/join')
+    "ng"       : require('./comandos/newGame'),
+    "join"     : require('./comandos/join'),
+    "tu"       : require('./comandos/tu')
 }
 
 async function readMensege(msg, bot) {
@@ -14,12 +15,14 @@ async function readMensege(msg, bot) {
         msg.reply(`Para me pedir algo o comando tem que começar com "${ConfigDs.prefix}"\n Exemplo: "${ConfigDs.prefix}ajuda" (que pode ser usar para saber mais sobre comandos)`);
         return;
     }
-    const cmd = msg.toString().replace(ConfigDs.prefix , "");
+
+    const param = msg.toString().replace(ConfigDs.prefix , "").split(" ");
+    const cmd = param.shift()
 
     if (commands[cmd]) {
-        commands[cmd](msg, bot);
+        commands[cmd](msg, param);
     }else{
-        msg.reply(`O comando "${msg.toString()}" não existe\n Caso tenha alguma dulvida sobre comandos use o comando ".ajuda"`);
+        msg.reply(`O comando "${ConfigDs.prefix + cmd}" não existe\n Caso tenha alguma dulvida sobre comandos use o comando ".ajuda"`);
     }
 }
 module.exports = readMensege;
